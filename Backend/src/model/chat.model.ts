61bcce3b63db21996ba1330ceb70dbd5f698053c
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-03-09 14:21:22 +0530

    chatchanges

--- Backend/src/model/chat.model.ts
+++ Backend/src/model/chat.model.ts
@@ -1,46 +1,55 @@
-import mongoose,{Schema,Document,Types} from 'mongoose';
+import mongoose, { Schema, Document, Types } from "mongoose";
 
-
-export interface IChat extends Document{
-    type : 'personal' | "group";
-    name :String; //group name
-    participants:Types.ObjectId[];
-    admins:Types.ObjectId[];
-    lastMessage:Types.ObjectId;
-    aiActiveUser?:Types.ObjectId[];
-    createdAt:Date;
-    updatedAt:Date;
-
+export interface IChat extends Document {
+  type: "personal" | "group";
+  name?: string;
+  participants: Types.ObjectId[];
+  admins?: Types.ObjectId[];
+  lastMessage?: Types.ObjectId;
+  aiActiveUsers?: Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-const chatSchema = new mongoose.Schema({
-    type:{
-        type:String,
-        enum:["personal" , "group"],
-        required:true,
+const chatSchema = new Schema<IChat>(
+  {
+    type: {
+      type: String,
+      enum: ["personal", "group"],
+      required: true,
     },
-    name:{
-        type:String,
-        trim:true,
-    },
-    participant:{
-        type:Schema.Types.ObjectId,
-        ref:"User",
-        required:true,
-    },
-    admins:[{
-        type:Schema.Types.ObjectId,
-        ref:"User",
-    }],
 
-    lastMessage:{
-        types:Schema.Types.ObjectId,
-        ref:"Message",
+    name: {
+      type: String,
+      trim: true,
     },
-    aiActiveUsers:[{
-        types:Schema.Types.ObjectId,
-        ref:"User",
-    }],
 
+    participants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
 
-},{timestamps:true})
+    admins: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    lastMessage: {
+      type: Schema.Types.ObjectId,
+      ref: "Message",
+    },
+
+    aiActiveUsers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
