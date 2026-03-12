568e3072b681aafc2efbe07fbdee0f71ac81c38a
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-03-12 23:20:29 +0530

    redis

---
+++ Backend/src/config/redis.ts
@@ -0,0 +1,16 @@
+import { Redis } from "ioredis";
+
+export const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
+  maxRetriesPerRequest: null,
+});
+
+redis.on("connect", () => {
+  console.log("✅ Redis connected");
+});
+
+redis.on("error", (err) => {
+  console.error("❌ Redis error:", err);
+});
