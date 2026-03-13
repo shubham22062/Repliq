87026c7a81d01f06c24cb4244ae995e66fb6fde7
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-03-13 21:51:45 +0530

    connection

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
 
 const app = express();
 
 const PORT = process.env.PORT||5000;
+
+connectDB();
 
 app.get('/', (req, res)=>{
     res.send("ai chat bckend is running");
