69a27d7c9ea1cfabdd1356e749cadb3e2d245034
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-03-25 22:32:55 +0530

    changes

--- Backend/src/model/user.model.ts
+++ Backend/src/model/user.model.ts
@@ -1,17 +1,26 @@
 import mongoose, { Schema } from 'mongoose';
+import { Interface } from 'node:readline';
+
+export interface IUser extends Document{
+    name:string;
+    password:string;
+    email:string;
+    aiModeEnabled:boolean;
+   aiPersonaPrompt:string;
+}
 
 
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema<IUser>({
     name:String,
     password:String,
     email:String,
     aiModeEnabled:{type:Boolean,default:false},
-    aiPersonaPropmpt:{type:String, defaulr:"reply causally..."},
+    aiPersonaPrompt:{type:String, default:"reply causally..."},
     
 
 
 },{timestamps:true})
 
- const User = mongoose.model("User",userSchema);
+ const User = mongoose.model<IUser>("User",userSchema);
  export default User
