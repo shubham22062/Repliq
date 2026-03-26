83f0492ba3d1ee20ab03324b4464546f0f4ccb70
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-03-26 21:41:26 +0530

    authroutes

--- Backend/src/routes/auth.routes.ts
+++ Backend/src/routes/auth.routes.ts
@@ -1,12 +1,33 @@
 import express from "express"
+import { signup, login } from "../services/auth.services.js";
 
 const router = express.Router();
 
 
-router.post("/signup" , (req,res)=>{
+router.post("/signup" , async(req,res)=>{
     console.log("THis is a signup route")
-})
+    try {
+        const {name ,email,password} = req.body;
+        const data = await signup(name , email , password);
 
-router.post("/Login", (req,res)=>{
+        res.json(data);
+    } catch (error:any) {
+        res.status(400).json({message:error.message})
+    }
+});
+
+router.post("/Login", async (req,res)=>{
     console.log("This is a login route ")
-})
+   try {
+     const {email,password} = req.body;
+     const data = await login(email ,password);
+     res.json(data);
+ 
+   } catch (error:any) {
+        res.status(400).json({message:error.message})
+   }
+
+});
+
+
+export default router;
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
-
-const app = express();
+import authRoutes from "./routes/auth.routes.js"
 
 const PORT = process.env.PORT||5000;
 
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes)
 connectDB();
 
 app.get('/', (req, res)=>{
