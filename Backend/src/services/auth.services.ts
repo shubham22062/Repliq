162197ea12613f9bae902fa53a38d389ebda547f
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-03-26 19:31:11 +0530

    jwt

--- Backend/src/services/auth.services.ts
+++ Backend/src/services/auth.services.ts
@@ -2,6 +2,40 @@ import bcrypt from "bcryptjs";
 import User from "../model/user.model.js";
 
 
-export const signup = async()=>{
-    console.log ("This is a singup function")
+export const signup = async(name:string, email:string, password:string)=>{
+    const existingUser = await User.findOne({email});
+
+    if(existingUser){
+        throw new Error("User already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+
+    })
+
+    
+ return user
+
+}
+
+
+export const login = async (email:string, password:string) =>{
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw new Error("Invalid credentials")
+    }
+ 
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if(!isMatch){
+        throw new Error("invalid credentials")
+    }
+ return user
+
 }
