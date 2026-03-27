0f69ffa080dd2ac7fb21cd28ed08c066a6311d0d
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-03-27 22:21:45 +0530

    this is chat model

--- Backend/src/model/message.model.ts
+++ Backend/src/model/message.model.ts
@@ -1,4 +1,12 @@
-import mongoose from "mongoose";
+import mongoose ,{Schema ,Document ,Types}from "mongoose";
+
+export interface IMessage extends Document{
+    chatId:Types.ObjectId;
+    senderId:Types.ObjectId;
+    content:string;
+    isAI?:boolean;
+    createdAt:Date;
+}
 
 const messageSchema = new mongoose.Schema({
     chatId:{type:mongoose.Types.ObjectId, ref:"Chat"},
